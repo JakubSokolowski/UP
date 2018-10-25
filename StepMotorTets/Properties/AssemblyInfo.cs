e35892daf9d63a3e5e3a7fd936746530f6fd689f
Jakub Sokolowski <jakub.sokolowski1996@gmail.com>
2018-10-25 20:17:16 +0200

    Add test project for StepMotor

---
+++ StepMotorTets/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StepMotorTets")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StepMotorTets")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad34dc42-eb3e-4458-ba48-8b82b7bbb78b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StepMotorTets/SignalSequenceTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using StepMotor;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace StepMotorTests
+{
+    [TestClass]
+    public class SignalSequenceTests
+    {
+        [TestMethod]
+        public void Create_GeneratesValidHalfStepRightSequence()
+        {
+            var sequence = ControlSequence.Create(ControlMode.HalfStep, Direction.Right, MotorID.First);
+            var expected = new List<byte>() { 0x05, 0x01, 0x09, 0x08, 0x0A, 0x02, 0x06, 0x04 };
+            CollectionAssert.AreEqual(expected, sequence.signalSequence);
+        }
+        [TestMethod]
+        public void Create_GeneratesValidHalfStepLeftSequence()
+        {
+            var sequence = ControlSequence.Create(ControlMode.HalfStep, Direction.Left, MotorID.First);
+            var expected = new List<byte>() { 0x05, 0x01, 0x09, 0x08, 0x0A, 0x02, 0x06, 0x04 };
+            expected.Reverse();
+            CollectionAssert.AreEqual(expected, sequence.signalSequence);
+        }
+        [TestMethod]
+        public void Create_GeneratesValidFullStepRightSequence()
+        {
+            var sequence = ControlSequence.Create(ControlMode.FullStep, Direction.Right, MotorID.First);
+            var expected = new List<byte>() { 0x01, 0x08, 0x02, 0x04 };
+            CollectionAssert.AreEqual(expected, sequence.signalSequence);
+        }
+        [TestMethod]
+        public void Create_GeneratesValidFullStepLeftSequence()
+        {
+            var sequence = ControlSequence.Create(ControlMode.FullStep, Direction.Left, MotorID.First);
+            var expected = new List<byte>() { 0x01, 0x08, 0x02, 0x04 };
+            expected.Reverse();
+            CollectionAssert.AreEqual(expected, sequence.signalSequence);
+        }
+
+        [TestMethod]
+        public void NextSignal_ReturnsValidSignalAfterRotation()
+        {
+            var sequence = ControlSequence.Create(ControlMode.HalfStep, Direction.Right, MotorID.First);
+            byte expected = 0x01;
+            byte actual = sequence.NextSignal(Direction.Right);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NextSignal_ReturnsValidSignalAfterMultipleRotations()
+        {
+            var sequence = ControlSequence.Create(ControlMode.HalfStep, Direction.Right, MotorID.First);
+            byte expected = 0x09;
+            byte actual = 0x05;
+            for(int i = 0; i < 10; i++)
+                 actual = sequence.NextSignal(Direction.Right);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NextSignal_ReturnsValidSignalAfterChangingRotationDirection()
+        {
+            var sequence = ControlSequence.Create(ControlMode.HalfStep, Direction.Right, MotorID.First);
+            byte expected = 0x01;
+            byte actual = sequence.NextSignal(Direction.Right);
+            Assert.AreEqual(expected, actual);
+            expected = 0x04;
+            actual = sequence.NextSignal(Direction.Left);
+            actual = sequence.NextSignal(Direction.Left);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NextSignal_ReturnsValidSignalAfterChangingRotationDirectionAndMultipleRotations()
+        {
+            var sequence = ControlSequence.Create(ControlMode.HalfStep, Direction.Right, MotorID.First);
+            byte expected = 0x02;
+            byte actual = 0x01;
+            for (int i = 0; i < 5; i++)
+                actual = sequence.NextSignal(Direction.Right);
+            Assert.AreEqual(expected, actual);
+
+            expected = 0x05;
+            for (int i = 0; i < 5; i++)
+                actual = sequence.NextSignal(Direction.Left);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
